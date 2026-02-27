dc59b74b78f85bfc569dd92a134612bb6beb8ad4
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-02-27 08:04:37 +0100

    feat(user): add an owner column to the redirects table

---
+++ migrations/20260226195045_add_owner_column_to_user_table.sql
@@ -0,0 +1,21 @@
+CREATE TEMPORARY TABLE IF NOT EXISTS redirects_temp AS
+SELECT
+    id,
+    alias,
+    url
+FROM redirects;
+
+DROP TABLE redirects;
+
+CREATE TABLE IF NOT EXISTS redirects (
+    id INTEGER NOT NULL PRIMARY KEY,
+    alias TEXT NOT NULL UNIQUE,
+    url TEXT NOT NULL,
+    owner TEXT, -- keeping it nullable for migration of redirects before ownership was introduced
+    FOREIGN KEY(owner) REFERENCES users(id)
+);
+
+INSERT INTO redirects(id, alias, url, owner)
+SELECT id, alias, url, NULL FROM redirects_temp;
+
+DROP TABLE redirects_temp;
