4c10ea2b17a20e048ef1f0eb46ada8f7c18f8722
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-03-02 22:16:23 +0100

    feat(user): adding route to change user password

--- src/controller/user.rs
+++ src/controller/user.rs
@@ -1,10 +1,35 @@
-use axum::{Router, http::StatusCode, response::IntoResponse, routing::post};
+use axum::{
+    Extension, Json, Router, debug_handler, extract::State, http::StatusCode,
+    response::IntoResponse, routing::patch,
+};
 
-use crate::AppContext;
+use crate::{
+    AppContext,
+    model::{PasswordChangeDataDTO, UserClaimsDTO, UserPasswordChangeDTO},
+    service::DbServiceError,
+};
 
 pub(crate) fn user_management_router() -> Router<AppContext> {
-    Router::new().route("/api/users/password", post(change_user_password_handler))
+    Router::new().route("/api/users/password", patch(change_user_password_handler))
 }
-async fn change_user_password_handler() -> impl IntoResponse {
-    StatusCode::NOT_IMPLEMENTED.into_response()
+#[debug_handler]
+async fn change_user_password_handler(
+    State(app_context): State<AppContext>,
+    Extension(user_claims): Extension<UserClaimsDTO>,
+    Json(passwords): Json<PasswordChangeDataDTO>,
+) -> impl IntoResponse {
+    let password_change = UserPasswordChangeDTO {
+        user_id: user_claims.user_id,
+        pw: passwords,
+    };
+    let res = app_context
+        .user_service
+        .change_user_pw(&password_change)
+        .await;
+    match res {
+        Ok(_) => StatusCode::OK,
+        Err(DbServiceError::NotFoundError) => StatusCode::NOT_FOUND,
+        Err(DbServiceError::AuthError(_)) => StatusCode::UNAUTHORIZED,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
 }
