66d1aa61202e00c800c0118cb140ecfd282ed7ec
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-03-04 11:37:26 +0100

    feat(user): new route /api/users/register for user registration

--- src/controller/user.rs
+++ src/controller/user.rs
@@ -1,18 +1,28 @@
 use axum::{
-    Extension, Json, Router, debug_handler, extract::State, http::StatusCode,
-    response::IntoResponse, routing::patch,
+    Extension, Json, Router, debug_handler,
+    extract::State,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{patch, post},
 };
 
 use crate::{
     AppContext,
-    model::{PasswordChangeDataDTO, UserClaimsDTO, UserPasswordChangeDTO},
+    model::{
+        PasswordChangeDataDTO, UserClaimsDTO, UserCredentialsDTO, UserPasswordChangeDTO,
+        UserRegistrationDTO,
+    },
     service::DbServiceError,
 };
 
-pub(crate) fn user_management_router() -> Router<AppContext> {
+pub(crate) fn protected_user_management_router() -> Router<AppContext> {
     Router::new().route("/api/users/password", patch(change_user_password_handler))
 }
-#[debug_handler]
+
+pub(crate) fn user_management_router() -> Router<AppContext> {
+    Router::new().route("/api/users/register", post(register_user_handler))
+}
+
 async fn change_user_password_handler(
     State(app_context): State<AppContext>,
     Extension(user_claims): Extension<UserClaimsDTO>,
@@ -29,6 +39,26 @@ async fn change_user_password_handler(
     match res {
         Ok(_) => StatusCode::OK,
         Err(DbServiceError::NotFoundError) => StatusCode::NOT_FOUND,
+        Err(DbServiceError::AuthError(_)) => StatusCode::UNAUTHORIZED,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
+#[debug_handler]
+async fn register_user_handler(
+    State(app_content): State<AppContext>,
+    Json(payload): Json<UserRegistrationDTO>,
+) -> impl IntoResponse {
+    let user_credentials = UserCredentialsDTO {
+        name: payload.name,
+        pw: payload.pw,
+    };
+    let res = app_content
+        .user_service
+        .register_user(&user_credentials, &payload.token)
+        .await;
+    match res {
+        Ok(_) => StatusCode::OK,
         Err(DbServiceError::AuthError(_)) => StatusCode::UNAUTHORIZED,
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
     }
