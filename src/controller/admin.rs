4fe4a334d857697e43fe704708d06dc3836175c8
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-03-06 15:11:07 +0100

    feat(admin): adding new route for admins to read out all redirects

--- src/controller/admin.rs
+++ src/controller/admin.rs
@@ -1,17 +1,28 @@
-use axum::{Json, Router, extract::State, http::StatusCode, response::IntoResponse, routing::get};
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{delete, get},
+};
 
-use crate::{AppContext, middleware};
+use crate::{AppContext, middleware, service::DbServiceError};
 
 pub(crate) fn router() -> Router<AppContext> {
     Router::new()
         .route(
             "/api/admin/registrationtoken",
-            get(request_user_registration_token),
+            get(request_user_registration_token_handler),
+        )
+        .route("/api/admin/redirects", get(get_all_redirects_admin_handler))
+        .route(
+            "/api/admin/redirects/{id}",
+            delete(delete_redirect_admin_handler),
         )
         .layer(axum::middleware::from_fn(middleware::is_admin_middleware))
 }
 
-async fn request_user_registration_token(
+async fn request_user_registration_token_handler(
     State(app_context): State<AppContext>,
 ) -> impl IntoResponse {
     let res = app_context
@@ -20,6 +31,31 @@ async fn request_user_registration_token(
         .await;
     match res {
         Ok(t) => (StatusCode::OK, Json(t)).into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, Json(e)).into_response(),
+    }
+}
+
+async fn get_all_redirects_admin_handler(
+    State(app_context): State<AppContext>,
+) -> impl IntoResponse {
+    let res = app_context.redirect_service.get_all_redirects().await;
+    match res {
+        Ok(t) => (StatusCode::OK, Json(t)).into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, Json(e)).into_response(),
+    }
+}
+
+async fn delete_redirect_admin_handler(
+    State(app_context): State<AppContext>,
+    Path(id): Path<String>,
+) -> impl IntoResponse {
+    let query = app_context
+        .redirect_service
+        .delete_redirect_by_id(&id)
+        .await;
+    match query {
+        Ok(()) => StatusCode::NO_CONTENT.into_response(),
+        Err(DbServiceError::NotFoundError) => StatusCode::NOT_FOUND.into_response(),
         Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, Json(e)).into_response(),
     }
 }
